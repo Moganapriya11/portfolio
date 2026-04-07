58aad812af1126f55ed6f8b6dd46d8b01da3d90d
Mogana_Priya.J <moganajanaki@gmail.com>
2026-04-07 17:25:06 +0530

    test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ function Hero() {
 
           {/* Title */}
           <h1 className="hero__title">
-            Building Modern2
+            Building Modern
             <br />
             <span className="hero__title-gradient">React Interfaces</span>
           </h1>
